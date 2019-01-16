9b46d86d54b32ce90cae8d7357edf6e07b088036
danny <danny@dannys-MacBook-Pro.local>
2019-01-16 18:43:04 -0500

    4

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -1,6 +1,7 @@
 ///GIT CHECK
 
 
+
 const {MongoClient , ObjectID} = require('mongodb');
 
 MongoClient.connect('mongodb://localhost:27017/TodoApp' ,  {useNewUrlParser: true} , (err,client)=>{
--- playground/mongodb-find.js
+++ playground/mongodb-find.js
@@ -1,3 +1,4 @@
+//git check
 
 const {MongoClient , ObjectID} = require('mongodb');
 
